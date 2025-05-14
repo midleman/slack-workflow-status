b274996e6f0af7983e5b2e5479db3c1d44c98799
Marie Idleman <marie.idleman@posit.co>
2025-05-14 12:08:58 -0500

    failures

--- tests/example-1.test.ts
+++ tests/example-1.test.ts
@@ -8,6 +8,9 @@ test('this test should fail', async () => {
   expect(1).toBe(2)
 })
 
+test('this test should fail too', async () => {
+  expect(1).toBe(2)
+})
 test('this test should flake', async ({}, testInfo) => {
   if (testInfo.retry) {
     expect(2).toBe(2)
